c4dcf76f7ab7d1e6e72fcc00d8996755e0468be1
RAHINI KALRA <rahini8584.beaift25@chitkara.edu.in>
2025-08-26 23:11:37 +0530

    Add files via upload

---
+++ swapno.py
@@ -0,0 +1,7 @@
+a=int(input("enter the first no."))
+b=int(input("enter the second no."))
+a=a+b
+b=a-b
+a=a-b
+print("after swaping the first no. becomes ",a)
+print("after swaping the second no. becomes ",b) 
