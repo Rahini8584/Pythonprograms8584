40ae91eecc08a24686de15638def63903ba54f95
RAHINI KALRA <rahini8584.beaift25@chitkara.edu.in>
2025-08-26 10:05:24 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,57 @@
+#arthmetic op. 
+    #a=10
+    #b=20
+    #c=a+b
+    #print(c)     #(sum)
+
+  #  a=12
+   # b=89
+    #c=b-a
+   # print(c)   #(subtraction)
+
+
+    #    a=12
+     #   b=3
+      #  c=a*b
+       # print(c)  #multiplication
+
+
+      #  a=45
+       # b=5
+       # c=a/b
+        #print(c)  #division 
+
+#    a=120
+ #   b=42
+  #  c=a%b
+   # print(c)    #modulus
+
+# assignment op 
+#"  =  , += ,-= "
+
+#    a=10
+#    a+=10
+#    print(a)
+
+#        a=18
+#        a-=9.56
+#        print(a)
+
+
+
+#logical op 
+#"   and  "
+#a=10.78
+#b=12.12
+#print(a<b and a<10)
+
+#"   or   "
+#x=74.589
+#y=36.00
+#print(x>y or x>100)
+
+# "  not   "
+#   a=45.23
+#   b=18.82
+#   print(not a>b,b>17)
+
