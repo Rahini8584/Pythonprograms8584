9e96954f17519d8ed37e7d7432f03098b183628c
RAHINI KALRA <rahini8584.beaift25@chitkara.edu.in>
2025-09-08 11:56:38 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,20 @@
+# n=int(input("enter the no. of rows"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#             if(j<=n-i):
+#                 print(" ",end=" ")
+#             else:
+#                 print("*",end=" ")
+#     print()
+
+#print circle 
+# for i in range(6):
+#     for j in range(1,7):
+#         if((i==0 or i==4) and(j==3 or j==4 or j==5)):
+#             print("*",end=" ")
+#         elif((i==1 or i==2 or i==3) and (j==2 or j==6)):
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
