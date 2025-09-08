646c99636ba872c75e38bec03a0381b2c905c237
RAHINI KALRA <rahini8584.beaift25@chitkara.edu.in>
2025-09-08 12:01:39 +0530

    Add files via upload

---
+++ functionBasic.py
@@ -0,0 +1,31 @@
+# A FUNCTION IS A BLOCK OF CODE THAT PERFORMS A SPECIFIC TASK.
+# DEF FUNCTION_NAME(PARAMETERS):
+    # FUNCTION BODY 
+    # RETURN VALUE
+
+# def greet(name):
+#     return (f"hello {name}")
+# print(greet("rahini"))
+
+
+# nums= [10,20,30]
+# print(len(nums))
+# print(max(nums))
+# print(min(nums))
+# print(sum(nums))
+
+# def square(x):
+#     return x*x
+# print(square(4))
+
+# a=int(input("enter the first  no."))
+# b=int(input("enter the  second no. "))
+# def add(a,b):
+#     return (a+b) 
+# print(add(a,b))
+
+a=65
+for i in range(26):
+
+    print(chr(a))
+    a+=1
---
+++ nested loop no.py
@@ -0,0 +1,21 @@
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("1",end=" ")
+#     print()
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+#     print()
+
+# a=1
+# for i in range(1,11):
+#     for j in range(1,i+1):
+#         print(a,end=" ")
+#         a+=1
+#     print()
+
+for i in range(6,0,-1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
